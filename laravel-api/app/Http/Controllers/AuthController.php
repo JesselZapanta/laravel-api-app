526448dba74d17f8bc1e3e7810a9a397e8867a07
JesselZapanta <jesselzapanta9@gmail.com>
2025-05-04 13:58:05 +0800

    auth api login, register and log out

--- laravel-api/app/Http/Controllers/AuthController.php
+++ laravel-api/app/Http/Controllers/AuthController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($data);
+
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'token' => $token->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();//first() - return a user oject, not an array
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'status' => 'Unauthorized'
+            ], 401); 
+        }
+
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 'Logout'
+        ], 200);
+    }
 }
